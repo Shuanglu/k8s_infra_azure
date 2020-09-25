44b069871a9a722279a6f3ad0174b17341f1c77b
Shuanglu <shuanglu1993@foxmail.com>
2020-09-25 16:06:48 +0800

    add scripts and templates

--- initilization/scripts/k8s_conf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-k8s_conf() {
-  if [ -s "/etc/kubernetes/azure.json"]; then
-    rm -rf /etc/kubernetes/azure.json
-    cp /var/log/scripts/conf/azure.json /etc/kubernetes/azure.json
-  fi
-  kubeadm init --config /var/log/scripts/conf/kubeadm.conf  
-}
